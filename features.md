e692c31b3cdf4241c963a5b3e6cd5ae7ba9c189f
Kohei Miyaguchi <koheimiyaguchi@gmail.com>
2019-07-05 13:58:21 +0900

    Setup required features

---
+++ features.md
@@ -0,0 +1,11 @@
+### Requirement
+It must be a tool that assists us to achieve our respective goals.
+To do so, it offers the functionalities of
+* Goal setting,
+* Action planning,
+* Progress monitoring.
+
+### Details
+* Goal setting and action planning are structured in eight different time spans,
+namely, daily, weekly, monthly, quarterly, yearly, triennial, decennial and life-scale.
+* The progress monitoring functionality allows us to record what we did in each time spans.
---
+++ setup.py
@@ -0,0 +1,43 @@
+import sys
+from setuptools import setup, find_packages
+
+CURRENT_PYTHON = sys.version_info[:2]
+MIN_PYTHON = (3, 6)
+
+if CURRENT_PYTHON < MIN_PYTHON:
+    sys.stderr.write("""
+        ============================
+        Unsupported Python Version
+        ============================
+        
+        Python {}.{} is unsupported. Please use a version newer than Python {}.{}.
+    """.format(*CURRENT_PYTHON, *MIN_PYTHON))
+    sys.exit(1)
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.readlines()
+
+with open('VERSION') as f:
+    VERSION = f.read().strip()
+
+with open('README.md') as f:
+    readme = f.readlines()
+
+files = ["resources/*"]
+
+setup(name='pommerman',
+      version=VERSION,
+      description=readme,
+      url='https://github.com/koheimiya/do-what-you-want',
+      author='Kohei Miyaguchi',
+      author_email='koheimiyaguchi@gmail.com',
+      license='MIT',
+      classifiers=[
+          'Programming Language :: Python :: 3',
+      ],
+      packages=find_packages(),
+      install_requires=install_requires,
+      entry_points={
+        'console_scripts': [],
+      },
+      zip_safe=False)
