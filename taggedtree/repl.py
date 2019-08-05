5b850d29bc18eac3ac3ac8c5a482fadf91eb7908
Kohei Miyaguchi <koheimiyaguchi@gmail.com>
2019-08-05 09:05:36 +0900

    minor

--- taggedtree/repl.py
+++ taggedtree/repl.py
@@ -1,13 +1,10 @@
 """ REPL for tagged ordered trees """
-from typing import Optional, Tuple, List
-
 import editor
 
-from taggedtree.core import save_tree, load_tree, show_tree, show_leaves, subtrees, show_attr, attributes, Tree, Attr, \
-    valid_priority, new_attr, valid_tag, root, new_tree, normalized_tree, sort_tree
+from taggedtree.core import save_tree, load_tree, show_tree, show_leaves, normalized_tree, sort_tree, read_tree
 
-GREETINGS = "---- TOT REPL ----"
-PROMPT = "TOT > "
+GREETINGS = "---- Running REPL on {} ----"
+PROMPT = "REPL > "
 HELP = "l: list current goals, s/S: show the entire tree, e: edit the tree, " +\
     "n/N: sort and normalize, w: save the tree, q: quit REPL"
 
@@ -26,14 +23,14 @@ def repl(fname: str):
 
     tree = load_tree(fname)
     config = {"tree": tree, "fname": fname, "quit": False}
-    print(GREETINGS)
+    print(GREETINGS.format(fname))
     while not config["quit"]:
         cmd = input(PROMPT)
         if cmd in global_commands:
             try:
                 global_commands[cmd](config)
             except Exception as e:
-                print("Failed.")
+                print("Failed:")
                 print(e)
         else:
             print(HELP)
@@ -48,93 +45,10 @@ def _show(config: dict, show_done: bool = False):
 
 
 def _edit(config: dict):
-    msg = dumps_tree(config["tree"]).encode("utf-8")
+    msg = show_tree(config["tree"], show_done=True).encode("utf-8")
     result = editor.edit(contents=msg).decode("utf-8")
-    config["tree"] = loads_tree(result)
-
-
-def dumps_tree(tree: Tree, indent="", buffer="", last=True) -> str:
-    buffer += indent + dumps_attr(attributes(tree)) + "\n"
-    n = len(subtrees(tree))
-    for i, st in enumerate(subtrees(tree)):
-        last_child = i == n - 1
-        buffer = dumps_tree(st, indent=indent + "   |", buffer=buffer, last=last_child)
-    if not last and n > 1:
-        buffer += indent + "\n"
-    return buffer
-
-
-def dumps_attr(attr: Attr) -> str:
-    label, priority, tags, done = attr["label"], attr["priority"], attr["tags"], attr["done"]
-    done_mark = "x" if done else " "
-    tag_label = ''.join(' #' + t for t in tags)
-    return f"- [{done_mark}{priority}] {label}{tag_label}"
-
-
-def loads_attr(s: str) -> Tuple[str, Optional[Attr]]:
-
-    def split_by(s: str, delim: List[str]) -> List[str]:
-        res = []
-        for d in delim:
-            i = s.find(d)
-            if i == -1:
-                break
-            res.append(s[:i])
-            s = s[i + len(d):]
-        res.append(s)
-        return res
-
-    delimiters = ["- [", "] "]
-    ss = split_by(s, delimiters)
-    if len(ss) == 1:
-        return ss[0], None
-    elif len(ss) != 3:
-        raise ValueError(f"Unrecognized token: {s} -> {ss}")
-    indent, done_priority, label_tags = ss
-    indent = indent.replace("|", " ").replace("+", " ")
-    done_mark, priority = done_priority[:1], done_priority[1:]
-
-    assert done_mark in " x", "invalid done_mark"
-    done = done_mark == "x"
-    assert valid_priority(priority), "invalid priority"
-    label, *tags = map(str.strip, label_tags.split("#"))
-    assert all(valid_tag(t) for t in tags), "invalid tags"
-    return indent, new_attr(label=label, priority=priority, tags=tags, done=done)
-
-
-def loads_tree(s: str, tree_stack: List[Tuple[Optional[str], Tree]] = None) -> Tree:
-    if tree_stack is None:
-        tree_stack = []
-
-    if s == "":
-        (_, root_tree), *_ = tree_stack
-        return root_tree
-
-    sp = s.find("\n")
-    if sp == -1:
-        sp = len(s) + 1
-    line = s[:sp]
-    s_next = s[sp + 1:]
-    indent, attr = loads_attr(line)
-
-    if attr is not None:
-        tree = new_tree(attr)
-
-        # find parent
-        if tree_stack:
-            prev_indent, _ = tree_stack[-1]
-            if not (indent.startswith(prev_indent) and indent != prev_indent):
-                while True:
-                    prev_indent, prev_tree = tree_stack.pop()
-                    if indent == prev_indent:
-                        break
-            _, parent = tree_stack[-1]
-            subtrees(parent).append(tree)
-
-        # append a node
-        tree_stack.append((indent, tree))
-
-    return loads_tree(s_next, tree_stack)
+    config["tree"] = read_tree(result)
+    _show(config, show_done=True)
 
 
 def _normalize(config: dict, eager=False):
--- taggedtree/test.py
+++ taggedtree/test.py
@@ -1,5 +1,4 @@
-from taggedtree.repl import loads_tree
-from .core import new_tree, new_attr, root, show_tree, sort_tree, save_tree, load_tree
+from .core import new_tree, new_attr, root, show_tree, read_tree, sort_tree, save_tree, load_tree
 
 
 def sample_tree():
@@ -39,6 +38,6 @@ def test_save_load_tree():
 def test_show_loads_tree():
     tree = sample_tree()
     print(show_tree(tree))
-    tree1 = loads_tree(show_tree(tree, show_done=True))
+    tree1 = read_tree(show_tree(tree, show_done=True))
     print(show_tree(tree1))
     assert tree == tree1
